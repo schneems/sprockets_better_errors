5780e466e7ccdc02e529aed973ebd9e43aa0d90e
schneems <richard.schneeman@gmail.com>
2013-11-18 12:45:45 -0600

    only raise errors in development

--- lib/sprockets_better_errors/sprockets_railtie.rb
+++ lib/sprockets_better_errors/sprockets_railtie.rb
@@ -3,7 +3,7 @@ module Sprockets
     config.after_initialize do |app|
       Sprockets::Rails::Helper.precompile         ||= app.config.assets.precompile
       Sprockets::Rails::Helper.assets             ||= app.assets
-      Sprockets::Rails::Helper.raise_asset_errors = !::Rails.env.production?
+      Sprockets::Rails::Helper.raise_asset_errors  = ::Rails.env.development?
     end
   end
 end
